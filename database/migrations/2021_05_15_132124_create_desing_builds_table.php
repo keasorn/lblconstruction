e8d9506ba3454ad13b2c7a255b2edb6d53afeba4
keas sorn <kea3rd@gmail.com>
2021-05-23 23:14:58 +0700

    add new two section more

--- database/migrations/2021_05_15_132124_create_desing_builds_table.php
+++ database/migrations/2021_05_15_132124_create_desing_builds_table.php
@@ -15,6 +15,17 @@ class CreateDesingBuildsTable extends Migration
     {
         Schema::create('desing_builds', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->string("design_bg");
+            $table->string("build_bg");
+            $table->string("design_build_bg");
+            $table->string("design_title");
+            $table->text("design_des");
+            $table->string("build_title");
+            $table->text("build_des");
+            $table->text("journey");
+            $table->bigInteger("created_by")->unsigned();
+            $table->foreign('created_by')->references('id')->on('users');
             $table->timestamps();
         });
     }
